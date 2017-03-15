d90b3a3c898cccaa322c852cd7b9821ea4a94e16
Benjamin Hab <bennihab@googlemail.com>
2017-03-15 14:58:08 +0100

    Implemented verbose output, option for relaytype

--- Sous_vide/Sous_vide.ino
+++ Sous_vide/Sous_vide.ino
@@ -5,9 +5,16 @@
 // Set the pins used
 Adafruit_MAX31865 max = Adafruit_MAX31865(9, 10, 12, 13);
 
+//card pin
 #define cardSelect 4
+//refernence resistor value of the MAX31865 breakout board
 #define RREF 430.0
+//relaypin
 #define relaypin 11
+//if verbose==1, establish serial connection and print info to serial monitor
+#define verbose 1
+//type of used relay: 'SSR' or 'STD'. Mainly determines the PWM frequency (std PWM frequency vs. 1/T)
+#define relaytype 'STD'
 
 //variables
 char filename[15];
@@ -20,9 +27,12 @@ float I;
 float K_P=26;
 float K_I=0.01;
 //stepwidth in ms
-int T=1000;
+int T=5000;
 
-unsigned long t=millis()+1000;
+//PWM variable
+int dt=0;
+
+unsigned long t=millis()+T;
 uint8_t j=0;
 
 //declare functions
@@ -37,16 +47,16 @@ int PID(float e,float T){
 
 //setup
 void setup() {
-  Serial.begin(115200);
-  //while(!Serial){};
-  Serial.println("\r\nAnalog logger test");
-  //pinMode(13, OUTPUT);
-
+  //establish serial connection, if verbose output is desired
+  if(verbose==1){
+    Serial.begin(115200);
+    while(!Serial){};
+    Serial.println("\r\n Begin setup...");
+  }
 
   // see if the card is present and can be initialized:
-  if (!SD.begin(cardSelect)) {
+  if (!SD.begin(cardSelect) && verbose==1) {
     Serial.println("Card init. failed!");
-    //error(2);
   }
 
   strcpy(filename, "ANALOG00.TXT");
@@ -60,62 +70,52 @@ void setup() {
   }
 
   logfile = SD.open(filename, FILE_WRITE);
-  if( ! logfile ) {
+  //print progress or error to Serial
+  if( ! logfile && verbose==1 ) {
     Serial.print("Couldnt create ");
     Serial.println(filename);
-    //error(3);
   }
-  Serial.print("Writing to ");
-  Serial.println(filename);
 
-  pinMode(13, OUTPUT);
+  if(verbose==1){
+    Serial.print("Writing to ");
+    Serial.println(filename);
+  }
+
+  //set pinmodes for relay and 2nd LED
+  pinMode(relaypin, OUTPUT);
   pinMode(8, OUTPUT);
-  Serial.println("Ready!");
+
 
   //initialize temperature sensor
-
-
-  Serial.println("Adafruit MAX31865 PT100 Sensor Test!");
   max.begin(MAX31865_2WIRE);
-  //setup output pin
-  pinMode(relaypin,OUTPUT);
-
+  if(verbose==1){
+    Serial.println("Ready!");
+  }
 
 }
 
 
-
 void loop() {
-  if(t<millis())
-  { t=millis()+T;
+  //check, if timer has expired
+  if(t<millis()){
+    //reset timer
+    t=millis()+T;
+    //flash LED
     digitalWrite(8, HIGH);
+    //read temperature and calculate control output
     y=max.temperature(100, RREF);
-    e=w-y;
-    u=PID(e,T);
+    u=PID(w-y,T);
+    //log time, PID out and temperature
     logfile.print(millis()); logfile.print(", "); logfile.print(u); logfile.print(", "); logfile.println(y);
-    Serial.print(millis()); Serial.print(", "); Serial.print(u); Serial.print(", "); Serial.println(y);
-    //write
-    digitalWrite(8, LOW);
-    //SSR-Version:
-    //analogWrite(relaypin,u);
-    //normal Relay version:
-    if(u){
-      digitalWrite(relaypin,HIGH);
+    if(verbose==1){
+      Serial.print(millis()); Serial.print(", "); Serial.print(u); Serial.print(", "); Serial.println(y);
     }
-    //write changes to card
-    if(j>=100)
-    {logfile.flush();
-      /*if(!logfile.flush()){ //check, if an error occurs
-      logfile.close();
-      if(!SD.begin(cardSelect)){
-        //if an error occurs on reopening the sdcard, light both leds and shutdown cooker
-        digitalWrite(8,HIGH);
-        digitalWrite(13,HIGH);
-        digitalWrite(relaypin,LOW);
-        while(1){
+    //switch off the LED
+    digitalWrite(8, LOW);
 
-        }
-      }*/
+    //write changes to card
+    if(j>=100){
+      logfile.flush();
       //retrieve new filename
       for (uint8_t i = 0; i < 100; i++) {
         filename[6] = '0' + i/10;
@@ -127,8 +127,34 @@ void loop() {
       }
       logfile=SD.open(filename, FILE_WRITE);
       j-=100;
-      }
+    }
     j++;
+
+    //write u to the relay
+    switch(relaytype){
+      //SSR-Version
+      case 'SSR':
+        analogWrite(relaypin,u);
+        break;
+
+      case 'STD':
+        //normal Relay low frequency PWM version
+        if(u){
+          //pulse width in milliseconds
+          dt=int(u*t/255);
+          //write pulse to relay
+          digitalWrite(relaypin,HIGH);
+          delay(dt);
+          digitalWrite(relaypin,LOW);
+        }
+        break;
+
+      default:
+        if(verbose==1){
+          Serial.println("incorrect input for variable relaytype");
+        }
+        break;
+    }
 
   }
 }
